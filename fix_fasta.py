1eee348c524ce9d07105915d1fcf9dbd54950636
Kishwar <kishwar.shafin@gmail.com>
2019-12-15 10:06:57 -0800

    make a stupid mistake, fix a stupid mistake

---
+++ fix_fasta.py
@@ -0,0 +1,57 @@
+import os
+from argparse import ArgumentParser
+from argparse import ArgumentDefaultsHelpFormatter
+from collections import defaultdict
+import shutil
+import time
+import os
+import sys
+import concurrent.futures
+from datetime import datetime
+"""
+THIS SCRIPT FIXES FASTA FILES THAT DOESN'T HAVE '>' BEFORE READ NAME
+"""
+
+
+def check_if_line_is_only_sequence(line):
+    line_list = list(line)
+    for elm in line_list:
+        if elm != 'A' or elm != 'C' or elm != 'G' or elm != 'T' or elm != 'N':
+            return False
+    return True
+
+
+def fix_fasta(input_fasta):
+    with open(input_fasta, "r") as ins:
+        array = []
+        for line in ins:
+            array.append(line)
+            if check_if_line_is_only_sequence(line) is False:
+                print(line)
+
+
+if __name__ == '__main__':
+    '''
+    Processes arguments and performs tasks.
+    '''
+    parser = ArgumentParser(
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        add_help=False
+    )
+    parser.add_argument(
+        "-i",
+        "--input_fasta",
+        type=str,
+        required=True,
+        help="Path to the input fasta file."
+    )
+    parser.add_argument(
+        "-o",
+        "--output_directory",
+        type=str,
+        required=False,
+        help="Path to output directory."
+    )
+    FLAGS, unparsed = parser.parse_known_args()
+    signal_file_names = get_all_signal_file_names(FLAGS.read_ids, FLAGS.summary_file)
+    parallel_method_master(signal_file_names, FLAGS.signal_directory, FLAGS.output_directory, FLAGS.threads)
